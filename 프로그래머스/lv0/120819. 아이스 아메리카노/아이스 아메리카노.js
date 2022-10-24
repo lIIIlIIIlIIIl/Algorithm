a86dd41c581299a6d7059cd99419b59f1ad27d61
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2022-10-24 13:54:08 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -0,0 +1,12 @@
+function solution(money) {
+    // 아아 = 5500
+    // money = 머슥이가 가지고 있는 돈
+    // 머슥이가 최대로 마실 수 있는 아메리카노의 잔 수와 남은 돈을 배열로 반환
+    // 최대로 마실 수  있는 아메리카노의 잔 수 -> money / 5500
+    // 남은 돈 -> money - (5500 * (money/5500))
+    
+    let answer = [];
+    answer[0] = parseInt(money / 5500);
+    answer[1] = money - (5500 * answer[0]);
+    return answer;
+}
