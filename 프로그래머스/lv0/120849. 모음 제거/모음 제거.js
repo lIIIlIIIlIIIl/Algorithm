95520a2028ca2ad332b9adcb7c564e52497bd9a5
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2022-11-12 00:40:14 +0900

    [level 0] Title: 모음 제거, Time: 0.09 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,12 +1,3 @@
 function solution(my_string) {
-    let collection=['a', 'e', 'i', 'o', 'u'];
-    let result = [];
-    
-    my_string.split('').map((el)=>{
-        for(let i=0; i<collection.length; i++){
-            if(el === collection[i]) return;
-        }
-        result.push(el);
-    })
-    return result.join('');
+    return my_string.replace(/[aeiou]/g, '');
 }
