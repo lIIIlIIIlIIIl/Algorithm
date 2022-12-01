cd1124b5162bfe038d0d36c9d42cbc84a9c135a0
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2022-12-01 10:31:46 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,12 +1,5 @@
 function solution(my_str, n) {
-    const result = [];
-    for(let i = 0 ; i < (my_str.length/n) ; i ++) {
-        let part = '';
-        for(let j = i*n ; j < i*n+n ; j ++) {
-            if(my_str[j]) part+=my_str[j];
-            else break;
-        }
-        result.push(part);
-    }
-    return result
+    let res = [];
+    for (let i = 0; i < my_str.length; i+=n) res.push(my_str.slice(i, i+n));
+    return res;
 }
