46785df6d1ae7362257581f48d954e13f6c11a11
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-01-13 20:05:29 +0900

    [unrated] Title: 기사단원의 무기, Time: 2.81 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,14 +1,4 @@
 function solution(number, limit, power) {
-    let measureArr = [1];
-    for(let i=2; i<=number; i++){
-        let count = 0;
-        for(let j=1; j<=i/2; j++){
-            if(i%j === 0) count++;
-        }
-        measureArr.push(count+1);
-    }
-    return measureArr.map((el) => {
-        return el > limit ? power : el;
-    }).reduce((acc, cur) => acc + cur, 0);
+    var answer = 0;
+    return answer;
 }
-
