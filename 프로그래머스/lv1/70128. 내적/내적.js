e0faa8c4ed455a279a7d0540a45b9b7263623d9e
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-02-03 11:11:03 +0900

    [level 1] Title: 내적, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,7 +1,4 @@
 function solution(a, b) {
-    let answer = 0;
-    a.map((el, idx) => {
-        answer += (a[idx]*b[idx]);
-    })
-    return answer;
+    let answer = a.reduce((acc, cur, idx) => acc += cur*b[idx], 0)
+    return answer;        
 }
