f8d2f0db3e29652d3ed37f994f634ea9b8e269c5
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-02-22 15:21:19 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.10 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,16 +1,4 @@
-function solution(n){
-    let biNum = String(n.toString(2)).split('').filter((el) => el === '1').length;
-    let answer = n+1;
-    let isTry = true;
-
-    while(isTry){
-        let biBigNum = String(answer.toString(2)).split('').filter((el) => el === '1').length;
-
-        if(biNum === biBigNum){
-             isTry = false;
-        }else{
-             answer++;
-        }
-    }
-    return answer
+function solution(n) {
+    var answer = 0;
+    return answer;
 }
