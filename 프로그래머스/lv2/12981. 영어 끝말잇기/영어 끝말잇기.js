e04e4d18f04fb91cd3f65b5a554900fc4d9c4f82
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-02-24 17:19:48 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -4,7 +4,6 @@ function solution(n, words) {
 
     
     for(let i=1; i<words.length; i++){
-        // 회수 = i/3 + 1
         let beforWord = stackWords[stackWords.length-1];
 
         if(stackWords.length === 0 || !(stackWords.includes(words[i])) && beforWord[beforWord.length-1] === words[i][0]){
