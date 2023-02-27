ebf3c6117c30b9056575c0b0ed067f6797b43da9
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-02-27 21:35:39 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -2,19 +2,11 @@ function solution(n,a,b)
 {
     let answer = 0;
 
-    while(n !== 1){
-        let nextANum = Math.round(a/2);
-        let nextBNum = Math.round(b/2);
+    while(a !== b){
+        a = Math.round(a/2);
+        b = Math.round(b/2);
 
         answer++;
-
-        if(nextANum === nextBNum){
-            break;
-        } else{
-            a = nextANum;
-            b = nextBNum;
-            n = n/2
-        }
     }
     return answer;
 }
