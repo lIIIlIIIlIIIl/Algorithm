5c2991a03938cfd07b945911d1b64c9cd5bf63bb
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-05-11 16:54:50 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,6 +1,62 @@
 function solution(s) {
-  return s.split(' ').map(el => {
-    if (el === '') return el;
-    return el[0].toUpperCase() + el.slice(1).toLowerCase();
-  }).join(' ');
+const arr = s.split(' ');
+
+return arr.map((str) => {
+    if(str === ''){
+        return str
+    }
+    return str[0].toUpperCase() + str.slice(1).toLowerCase();
+}).join(' ')
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function solution(s) {
+//   return s.split(' ').map(el => {
+//     if (el === '') return el;
+//     return el[0].toUpperCase() + el.slice(1).toLowerCase();
+//   }).join(' ');
+// }
