f41ebe9320b1869f162a0203f433ff3ec6ba1b00
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-05-11 23:20:04 +0900

    [level 2] Title: 구명보트, Time: 11.60 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,21 +1,82 @@
-function solution(people, limit) {
-    people.sort((a,b) => b-a);
+function solution(people, limit){
+    const sortPeople = people.sort((a, b) => b-a);
+    let count = 0;
+    let start = 0;
+    let end = sortPeople.length - 1;
 
-    let answer = 0;
-    let high = 0;
-    let low = people.length-1;
-
-    while(high < low){
-        if(people[high]+people[low] > limit){
-            high++;
+    while(start < end){
+        if(sortPeople[start] + sortPeople[end] > limit){
+            start += 1;
         }else{
-            high++;
-            low--;
+            start += 1;
+            end -= 1;
         }
-        answer++;
+        count += 1
     }
 
-    if(high === low) answer++;
+    if(start === end) count += 1;
 
-    return answer;
+    return count;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function solution(people, limit) {
+//     people.sort((a,b) => b-a);
+
+//     let answer = 0;
+//     let high = 0;
+//     let low = people.length-1;
+
+//     while(high < low){
+//         if(people[high]+people[low] > limit){
+//             high++;
+//         }else{
+//             high++;
+//             low--;
+//         }
+//         answer++;
+//     }
+
+//     if(high === low) answer++;
+
+//     return answer;
+// }
