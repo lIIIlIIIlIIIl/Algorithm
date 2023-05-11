fbb9aef7de6ab64753940253e4ff9c214695a1b1
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-05-11 19:22:29 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,4 +1,80 @@
-function solution(n) {
-    var answer = 0;
-    return answer;
+function solution(n){
+    /*
+        n의 다음 큰 숫자는 n보다 큰 자연수
+        n의 다음 큰 숫자와 n은 2진수로 변환했을 때 1의 갯수가 같다.
+        n의 다음 큰 숫자는 조건 1,2를 만족하는 수 중에 가장 작은 수
+    */
+   let countOne = 0;
+   let countBiN = 0;
+   n.toString(2).split('').map((num) => {
+    if(num === '1') countBiN += 1;
+   });
+
+   while(n){
+    n += 1;
+    n.toString(2).split('').map((num) => {
+        if(num === '1') countOne += 1;
+       });
+
+    if(countOne === countBiN) return n;
+
+    countOne = 0;
+   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function solution(n){
+//     let biNum = String(n.toString(2)).split('').filter((el) => el === '1').length;
+//     let answer = n+1;
+//     let isTry = true;
+
+//     while(isTry){
+//         let biBigNum = String(answer.toString(2)).split('').filter((el) => el === '1').length;
+
+//         if(biNum === biBigNum){
+//              isTry = false;
+//         }else{
+//              answer++;
+//         }
+//     }
+//     return answer
+// }
