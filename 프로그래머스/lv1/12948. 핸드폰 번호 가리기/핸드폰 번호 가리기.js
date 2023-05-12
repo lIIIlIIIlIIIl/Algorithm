8d09bdf569a3b3806e00eaa6b4a90a34fe888e5b
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-05-12 15:31:47 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,15 +1,76 @@
+// 전화번호의 뒷 4자리를 제외한 나머지 숫자 전부를 *으로 가린 문자열 반환
+
 function solution(phone_number) {
-    let answer = '';
-    let num = phone_number.split("");
-    let numberLength = num.length;
+    let needNumber = phone_number.length-4;
     
-    for(let i=0; i<numberLength; i++){
-        if(i<numberLength-4){
-            answer+='*';
-        } else{
-            answer+=num[i];
-        }   
-    }
-    
-    return answer;
+    return phone_number.split('').map((num, idx) => {
+        if(idx < needNumber){
+            return '*'
+        }
+        return phone_number[idx];
+    }).join('')
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function solution(phone_number) {
+//     let answer = '';
+//     let num = phone_number.split("");
+//     let numberLength = num.length;
+    
+//     for(let i=0; i<numberLength; i++){
+//         if(i<numberLength-4){
+//             answer+='*';
+//         } else{
+//             answer+=num[i];
+//         }   
+//     }
+    
+//     return answer;
+// }
