0dc419a1974aa304e84c2d6263010475aac83c61
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-05-13 01:38:13 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 933.28 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,22 +1,10 @@
-const solution = (elements) => {
-    const sumSet = new Set();
-    
-    const len = elements.length;
-    for (let i=1; i<=len; i++) { 
-        let sum = 0;
-        for (let j=0; j<len; j++) { 
-            if (j === 0) { 
-                for (let k=0; k<i; k++) {
-                    sum += elements[k];
-                }
-            }
-            else { 
-                sum -= elements[j-1];
-                sum += elements[(j+i-1) % len];
-            }
-            sumSet.add(sum);
-        }
+function solution(elements) {
+  const set = new Set();
+  for (let i = 1; i <= elements.length; i++) {
+    const els = elements.concat(elements.slice(0, i));
+    for (let j = 0; j < elements.length; j++) {
+      set.add(els.slice(j, j + i).reduce((a, c) => a + c, 0));
     }
-    
-    return sumSet.size;
+  }
+  return set.size;
 }
