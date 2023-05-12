042e8436887aac97bca5dc59e035a0701cf5466c
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-05-12 15:27:50 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,8 +1,60 @@
-function solution(seoul) {
+function solution(seoul){
+    let placeNum;
     
-    let position = seoul.indexOf("Kim");
+    seoul.map((name, idx) => {
+        if(name === 'Kim'){
+            return placeNum = idx;
+        }
+    })
     
-    let answer = `김서방은 ${position}에 있다`
-    
-    return answer;
+    return `김서방은 ${placeNum}에 있다`
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function solution(seoul) {
+    
+//     let position = seoul.indexOf("Kim");
+    
+//     let answer = `김서방은 ${position}에 있다`
+    
+//     return answer;
+// }
