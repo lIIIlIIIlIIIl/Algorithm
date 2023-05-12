c8dd71c0018917ba10f7c26e5d58a63f4984745d
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-05-12 15:22:12 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -1,13 +1,61 @@
-function solution(n) {
-    let answer = [];
-    
-    let newArray = n.toString().split("");
-    let arrLength = newArray.length;
-    
-    for(let i=1; i<=newArray.length; i++){
-        let newElement = Number(newArray[arrLength-i]);
-        answer.push(newElement);
-    }
+function solution(n){
+    // 자연수를 뒤집어서 각 자리 숫자를 원소를 가지는 배열 반환
+    let answer = String(n).split('').map((num) => Number(num))
+    answer.reverse();
     
     return answer;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function solution(n) {
+//     let answer = [];
+    
+//     let newArray = n.toString().split("");
+//     let arrLength = newArray.length;
+    
+//     for(let i=1; i<=newArray.length; i++){
+//         let newElement = Number(newArray[arrLength-i]);
+//         answer.push(newElement);
+//     }
+    
+//     return answer;
+// }
