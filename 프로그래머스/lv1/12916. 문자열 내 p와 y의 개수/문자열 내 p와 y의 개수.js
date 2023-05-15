043040bc89735a49a259947384ec5969b73ebc80
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-05-15 13:50:15 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,17 +1,62 @@
-function solution(s){    
-    let str = s.toLowerCase();
-    
-    let countP = 0;
-    let countY = 0;
-    
-    str.split("").map((alp) => {
-        if(alp === 'p'){
-            countP += 1;
-        }
-        if(alp === 'y'){
-            countY += 1;
-        }
-    })
+function solution(s){
+    const problemStr = s.toLowerCase().split('');
+    const countP = problemStr.filter((str) => str === 'p').length;
+    const countY = problemStr.filter((str) => str === 'y').length;
 
-    return countP === countY ? true : false;
+    if(countP !== countY) return false
+    return true;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function solution(s){    
+//     let str = s.toLowerCase();
+    
+//     let countP = 0;
+//     let countY = 0;
+    
+//     str.split("").map((alp) => {
+//         if(alp === 'p'){
+//             countP += 1;
+//         }
+//         if(alp === 'y'){
+//             countY += 1;
+//         }
+//     })
+
+//     return countP === countY ? true : false;
+// }
