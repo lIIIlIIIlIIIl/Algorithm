8664e8bfdf80bc1d97012f057ed51c75745d08bf
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-05-15 11:25:51 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,19 +1,74 @@
 function solution(d, budget) {
-    let totalPrice = d.reduce((acc,cur)=>acc+cur,0);
-    let sortD = d.sort((a,b) => b-a);
     let answer = 0;
-    
-    if(totalPrice <= budget){
-        return d.length;
-    };
-    
-    
-    for(let i=0; i<sortD.length; i++){
-        if(totalPrice > budget){
-            totalPrice-=sortD[i];
-        } else{
-            answer++;
+    let sortD = d.sort((a,b) => a-b);
+    let sumD = 0;
+
+    sortD.forEach(element => {
+        sumD += element;
+
+        if(sumD <= budget){
+            answer += 1;
         }
-    }
+    });
     return answer;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function solution(d, budget) {
+//     let totalPrice = d.reduce((acc,cur)=>acc+cur,0);
+//     let sortD = d.sort((a,b) => b-a);
+//     let answer = 0;
+    
+//     if(totalPrice <= budget){
+//         return d.length;
+//     };
+    
+    
+//     for(let i=0; i<sortD.length; i++){
+//         if(totalPrice > budget){
+//             totalPrice-=sortD[i];
+//         } else{
+//             answer++;
+//         }
+//     }
+//     return answer;
+// }
