33af982a1d06be4e8cd7d6c82d361f3233965d9a
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-05-15 14:25:15 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,8 +1,55 @@
 function solution(s) {
-    let strLength = s.length;
-    
-    if(strLength % 2 === 0){
-        return s[(strLength/2)-1]+s[strLength/2];
-    }
-    return s[Math.floor(strLength/2)];
+    let evenOrOdd = s.length % 2 === 0 ? 'even' : 'odd';
+
+    if(evenOrOdd === 'odd') return s[parseInt(s.length / 2)];
+    if(evenOrOdd === 'even') return s[parseInt(s.length / 2) - 1] + s[parseInt(s.length / 2)];
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function solution(s) {
+//     let strLength = s.length;
+    
+//     if(strLength % 2 === 0){
+//         return s[(strLength/2)-1]+s[strLength/2];
+//     }
+//     return s[Math.floor(strLength/2)];
+// }
