2cf5214742fb2dcc30aa2e8f58d5a133263204a9
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-05-15 14:19:47 +0900

    [level 1] Title: 음양 더하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,10 +1,56 @@
 function solution(absolutes, signs) {
     let answer = 0;
-    absolutes.map((el, idx) => {
-        if(signs[idx]){
-            return answer += absolutes[idx]
-        }
-        return answer -= absolutes[idx]
-    })
+
+    for(let i=0; i<absolutes.length; i++){
+        signs[i] ? answer += absolutes[i] : answer -= absolutes[i];
+    }
+
     return answer;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function solution(absolutes, signs) {
+//     let answer = 0;
+//     absolutes.map((el, idx) => {
+//         if(signs[idx]){
+//             return answer += absolutes[idx]
+//         }
+//         return answer -= absolutes[idx]
+//     })
+//     return answer;
+// }
