75564531162077788b5b24dbb964e2cc4ba54fe2
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-05-15 23:56:53 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.40 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,11 +1,33 @@
 function solution(numbers) {
     let answer = [];
+
     for(let i=0; i<numbers.length; i++){
         for(let j=i+1; j<numbers.length; j++){
-            if(!answer.includes(numbers[i]+numbers[j])){
-                answer.push(numbers[i]+numbers[j])
-            }
+            let sumNum = numbers[i] + numbers[j];
+
+            if(!answer.includes(sumNum)) answer.push(sumNum);
         }
     }
-    return answer.sort((a,b)=>a-b);
+    return answer.sort((a,b) => a-b);
 }
+
+
+
+
+
+
+
+
+
+
+// function solution(numbers) {
+//     let answer = [];
+//     for(let i=0; i<numbers.length; i++){
+//         for(let j=i+1; j<numbers.length; j++){
+//             if(!answer.includes(numbers[i]+numbers[j])){
+//                 answer.push(numbers[i]+numbers[j])
+//             }
+//         }
+//     }
+//     return answer.sort((a,b)=>a-b);
+// }
