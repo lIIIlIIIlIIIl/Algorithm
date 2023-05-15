713b3dca1c3fa2c4ac26144bd5438dc482d1a446
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-05-15 12:00:11 +0900

    [unrated] Title: 콜라 문제, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,12 +1,40 @@
+function returnCoke(reqCoke, coke, resCoke){
+    return coke * parseInt(resCoke / reqCoke);
+}
+
 function solution(a, b, n) {
     let answer = 0 ;
     
     while(n >= a){
-        let bringBotle = parseInt(n / a);
-        let returnBotle = bringBotle * b;
+        let returnBotle = returnCoke(a, b, n)
         
         answer += returnBotle;
         n = returnBotle + (n % a);
     }
     return answer;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function solution(a, b, n) {
+//     let answer = 0 ;
+    
+//     while(n >= a){
+//         let bringBotle = parseInt(n / a);
+//         let returnBotle = bringBotle * b;
+        
+//         answer += returnBotle;
+//         n = returnBotle + (n % a);
+//     }
+//     return answer;
+// }
