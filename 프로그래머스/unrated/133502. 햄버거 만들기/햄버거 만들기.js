3085f5c7d8af86d69044998f9f8795ebe31046b9
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-05-23 23:45:55 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,14 +1,18 @@
 function solution(ingredient) {
-  let answer = 0;
-  const stack = [];
-
-  for (let i of ingredient) {
-    stack.push(i);
-
-    if (stack.slice(-4).join('') == '1231') {
-      stack.splice(-4);
-      answer++;
+    let stk = [];
+    let count = 0;
+    
+    for (let i = 0; i < ingredient.length; i++) {
+        stk.push(ingredient[i]);
+        if (
+            stk[stk.length-1] === 1 &&
+            stk[stk.length-2] === 3 &&
+            stk[stk.length-3] === 2 &&
+            stk[stk.length-4] === 1
+        ) {
+            count++;
+            stk.splice(-4);
+        }
     }
-  }
-  return answer;
+    return count;
 }
