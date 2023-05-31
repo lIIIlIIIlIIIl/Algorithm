ce95664a0af083555c5c7a0cdcba6fe4e0907538
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-05-31 18:03:11 +0900

    [unrated] Title: 리스트 자르기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,24 @@
+function solution(n, slicer, num_list) {
+    let a = slicer[0];
+    let b = slicer[1];
+    let c = slicer[2];
+    
+    if(n === 1){
+        return num_list.slice(0, b+1);
+    }
+    if(n === 2){
+        return num_list.slice(a);
+    }
+    if(n === 3){
+        return num_list.slice(a, b+1);
+    }
+    if(n === 4){
+        let answer = [];
+        let numList = num_list.slice(a, b+1);
+        for(let i=0; i<numList.length; i+=c){
+            answer.push(numList[i]);
+        }
+        return answer;
+    }
+    
+}
