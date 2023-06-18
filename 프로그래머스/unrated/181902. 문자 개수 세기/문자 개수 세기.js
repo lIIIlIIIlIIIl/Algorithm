4db2fb206a1f081c4741e7bd9cc05cb5428153c9
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-06-18 23:31:25 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let myString = [...my_string];
+    let answer = [];
+    
+    for(let i=0; i<alphabet.length; i++){
+        let count = myString.filter((el) => el === alphabet[i]).length;
+        answer.push(count);
+    }
+    return answer;
+}
