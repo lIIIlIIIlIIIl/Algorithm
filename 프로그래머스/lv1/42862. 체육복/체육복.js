51752d0ca224e62c50d5aadf4b6dd17cc7507f9c
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-07-10 23:09:37 +0900

    [level 1] Title: 체육복, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,32 +1,18 @@
 function solution(n, lost, reserve) {
+    const clothes = Array(n).fill(1);
 
-    let answer = 0;
+    lost.map((lost) => {clothes[lost-1] = 0});
+    reserve.map(reserve => {clothes[reserve-1] += 1});
 
-    let hasUniform = new Array(n).fill(1);
-
-    for (let i = 0; i < lost.length; i++) {
-        hasUniform[lost[i] - 1]--;
-    }
-
-    for (let i = 0; i < reserve.length; i++) {
-        hasUniform[reserve[i] - 1]++;
-    }
-
-    for (let i = 0; i < hasUniform.length; i++) {
-        if (hasUniform[i] === 0) {
-            if (hasUniform[i - 1] === 2) {
-                hasUniform[i]++;
-                hasUniform[i - 1]--;
-            } else if (hasUniform[i + 1] === 2) {
-                hasUniform[i]++;
-                hasUniform[i + 1]--;
-            }
+    for(let i=0; i<n; i++){
+        if(clothes[i] === 0 && clothes[i-1] ===2){
+            clothes[i] = 1;
+            clothes[i-1] = 1;
         }
-
-        if (hasUniform[i] >= 1) {
-            answer++;
+        else if(clothes[i] === 0 && clothes[i+1] === 2){
+            clothes[i] = 1;
+            clothes[i+1]=1;
         }
     }
-
-    return answer;
+    return clothes.filter(c => c > 0).length;
 }
