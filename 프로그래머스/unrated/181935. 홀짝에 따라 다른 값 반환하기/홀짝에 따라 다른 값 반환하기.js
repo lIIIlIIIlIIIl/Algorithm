5afe336a4b095b6963a5cb1e42da13b10e95b37a
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-08-25 21:03:09 +0900

    [unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,19 +1,19 @@
 function solution(n) {
     const isOdd = n % 2 === 1 ? true : false;
     
+    let answer = 0;
+    
     if(isOdd){
-        let answer = 0;
         for(let i=1; i<=n; i++){
             if(i % 2 === 1) answer += i;
         }
-        return answer;
     }
     
     if(!isOdd){
-        let answer = 0;
         for(let i=1; i<=n; i++){
             if(i % 2 === 0) answer += i**2;
         }    
-        return answer;
     }
+    
+    return answer;
 }
