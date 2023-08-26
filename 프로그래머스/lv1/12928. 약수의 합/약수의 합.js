fe447a13c420c2869b45b26633216daffaf18efc
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-08-26 15:51:53 +0900

    [level 1] Title: 약수의 합, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,10 +1,7 @@
-function solution(n) {
-    var answer = 0;
-    // 약수의 합을 구하시오
-    for(let i=1; i<=n; i++){
-        if(n%i === 0){
-            answer+=i
-        }
-    }
-    return answer;
+function solution(n){
+  let answer = 0;
+  for(let i=1; i<=n; i++){
+    if(n % i === 0) answer += i;
+  }
+  return answer;
 }
