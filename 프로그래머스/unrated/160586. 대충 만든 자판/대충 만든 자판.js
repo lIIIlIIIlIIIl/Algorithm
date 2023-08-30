4440bb760f4f60b095ff4a69f308d97b71febbb1
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-08-30 17:30:27 +0900

    [unrated] Title: 대충 만든 자판, Time: 0.96 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,19 +1,46 @@
+// function solution(keymap, targets) {
+//     let answer = [];
+//     let map = new Map();
+
+//     for(let key of keymap){
+//         for(let i=0; i<key.length; i++){
+//             if(!map.has(key[i]) || i+1 < map.get(key[i])) map.set(key[i], i+1);
+//         }
+//     }
+
+//     for (const target of targets) {
+//         let count = 0;
+//         for (let i = 0; i < target.length; i++) {
+//           count += map.get(target[i]);
+//         }
+//         answer.push(count || -1);
+//       }
+//     return answer;
+// }
+
 function solution(keymap, targets) {
-    let answer = [];
-    let map = new Map();
+    const answer = [];
+    const map = new Map();
 
-    for(let key of keymap){
-        for(let i=0; i<key.length; i++){
-            if(!map.has(key[i]) || i+1 < map.get(key[i])) map.set(key[i], i+1);
-        }
-    }
+    keymap.forEach((keys) => {
+        const targetArr = [...keys];
+        targetArr.forEach((key, idx) => {
+            if(!map.has(key) || idx + 1 < map.get(key)){
+                map.set(key, idx + 1);
+            }
+        })
+    })
 
-    for (const target of targets) {
+    targets.forEach((target) => {
+        const targetArr = [...target];
         let count = 0;
-        for (let i = 0; i < target.length; i++) {
-          count += map.get(target[i]);
-        }
+
+        targetArr.forEach((targetKey) => {
+            count += map.get(targetKey)
+        })
+
         answer.push(count || -1);
-      }
+    })
+
     return answer;
 }
