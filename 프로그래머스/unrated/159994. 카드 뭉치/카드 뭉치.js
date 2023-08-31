126d586413c436ae12101525c8171d86e49b0546
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-08-31 23:28:22 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,18 +1,29 @@
-function solution(cards1, cards2, goal) {
-    let answer = '';
+// function solution(cards1, cards2, goal) {
+//     goal.forEach((card) => {
+//         if(cards1[0] === card){
+//             cards1.splice(0,1);
+//         }
+//         if(cards2[0] === card){
+//             cards2.splice(0,1)
+//         }
+//         if(cards1[0] !== card && cards2[0] !== card){
+//             console.log(123)
+//             return 'No'
+//         }
+//     });
 
+//     return 'Yes'
+// }
+
+function solution(cards1, cards2, goal) {
+    let answer = 'Yes'
     goal.forEach((card) => {
-        if(cards1[0] !== card && cards2[0] !== card){
-            answer = 'No'
+        if(cards1[0] === card) cards1.shift();
+        else if(cards2[0] === card) cards2.shift();
+        else if(cards1[0] !== card && cards2[0] !== card){
+            answer = 'No';
             return;
         }
-        if(cards1[0] === card){
-            cards1.splice(0,1);
-        }
-        if(cards2[0] === card){
-            cards2.splice(0,1)
-        }
-    });
-
-    return answer === "" ? 'Yes' : answer;
+    })
+    return answer;
 }
