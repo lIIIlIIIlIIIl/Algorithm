39540e92dec25a72ed1da915bc8b0a7a6db4a506
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-09-01 17:54:46 +0900

    [unrated] Title: 둘만의 암호, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -1,12 +1,12 @@
 function solution(s, skip, index) {
     let answer = '';
-    let skipArr = skip.split('');
-    let filterArr = 'abcdefghijklmnopqrstuvwxyz'.split('').filter((str) => !skipArr.includes(str));
-
-    for(let i=0; i<s.length; i++){
-        let nextIndex = (filterArr.indexOf(s[i]) + index) % filterArr.length;
-        answer += filterArr[nextIndex];
-    }
-
+    const sArr = [...s]
+    const skipArr = [...skip];
+    const filterArr = 'abcdefghijklmnopqrstuvwxyz'.split('').filter((str) => !skipArr.includes(str));
+    
+    sArr.forEach((word, idx) => {
+        const nextIdx = (filterArr.indexOf(word) + index) % filterArr.length;
+        answer += filterArr[nextIdx];
+    })
     return answer;
 }
