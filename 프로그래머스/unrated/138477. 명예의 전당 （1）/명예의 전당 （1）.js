561e79143018eef6745f5bfe64e550a9a3520068
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-09-05 18:13:05 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,20 +1,37 @@
+// function solution(k, score) {
+//     let hallOfFame = [];
+//     let lowerArr = [];
+    
+//     for(let i=0; i<score.length; i++){
+//         if(i > k-1){
+//             if(score[i] > hallOfFame[0]){
+//                 hallOfFame.shift();
+//                 hallOfFame.push(score[i]);
+//             }
+//         }
+//         else{
+//             hallOfFame.push(score[i]);
+//         }
+//         hallOfFame.sort((a,b) => a-b);
+//         lowerArr.push(hallOfFame[0]);
+//     }
+    
+//     return lowerArr;
+// }
+
 function solution(k, score) {
-    let hallOfFame = [];
-    let lowerArr = [];
-    
-    for(let i=0; i<score.length; i++){
-        if(i > k-1){
-            if(score[i] > hallOfFame[0]){
-                hallOfFame.shift();
-                hallOfFame.push(score[i]);
-            }
+    const stack = []
+    return score.reduce((a,c) => {
+        if(stack.length < k) {
+            stack.push(c)
+            stack.sort((a,b) => a - b)
         }
-        else{
-            hallOfFame.push(score[i]);
+        else {
+            stack.push(c)
+            stack.sort((a,b) => a - b)
+            stack.shift()
         }
-        hallOfFame.sort((a,b) => a-b);
-        lowerArr.push(hallOfFame[0]);
-    }
-    
-    return lowerArr;
+        a.push(stack[0])
+        return a
+    },[])
 }
