8f46baab5febf54dadddd1a12b4f5fcd87369e88
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-09-13 23:58:28 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 2.18 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,18 +1,29 @@
-function solution(s) {
-    let answer = [];
-    let stackArr = [];
+// function solution(s) {
+//     let answer = [];
+//     let stackArr = [];
     
-    for(let i=0; i<s.length; i++){
-        if(!stackArr.includes(s[i])){
-            stackArr.push(s[i]);
-            answer.push(-1);
-            continue;
-        }
-        if(stackArr.includes(s[i])){
-            answer.push(stackArr.length - stackArr.lastIndexOf(s[i]));
-            stackArr.push(s[i]);
-            continue;
-        }
-    }
-    return answer;
+//     for(let i=0; i<s.length; i++){
+//         if(!stackArr.includes(s[i])){
+//             stackArr.push(s[i]);
+//             answer.push(-1);
+//             continue;
+//         }
+//         if(stackArr.includes(s[i])){
+//             answer.push(stackArr.length - stackArr.lastIndexOf(s[i]));
+//             stackArr.push(s[i]);
+//             continue;
+//         }
+//     }
+//     return answer;
+// }
+
+
+function solution(s) {
+    const hash={};
+
+    return [...s].map((v,i)=>{
+        let result = hash[v] !== undefined ? i - hash[v] : -1;
+        hash[v] = i;
+        return result;
+    });
 }
