1a444335b06aea95dd4e63eef14957e9807c0ef6
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-09-14 18:31:36 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,15 +1,24 @@
-function solution(food) {
-    let answerArr = [];
-    let canUseFood = food.map((el) => {
-        return el % 2 === 1 ? el - 1 : el;
-    })
+// function solution(food) {
+//     let answerArr = [];
+//     let canUseFood = food.map((el) => {
+//         return el % 2 === 1 ? el - 1 : el;
+//     })
     
-    for(let i=1; i<canUseFood.length; i++){
-        if(canUseFood[i] !== 0){
-            for(let j=0; j<canUseFood[i]/2; j++){
-                answerArr.push(i)
-            }
-        }
+//     for(let i=1; i<canUseFood.length; i++){
+//         if(canUseFood[i] !== 0){
+//             for(let j=0; j<canUseFood[i]/2; j++){
+//                 answerArr.push(i)
+//             }
+//         }
+//     }
+//     return answerArr.join('') + '0' + answerArr.slice(0).sort((a,b) => b-a).join('')
+// }
+
+function solution(food) {
+    let res = '';
+    for (let i = 1; i < food.length; i++) {
+        res += String(i).repeat(Math.floor(food[i]/2));
     }
-    return answerArr.join('') + '0' + answerArr.slice(0).sort((a,b) => b-a).join('')
+
+    return res + '0' + [...res].reverse().join('');
 }
