2fdcb30728ec7e9efa4e133786f796a18dab4cfc
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-09-16 15:30:42 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,37 +1,59 @@
-function solution(X,Y){
-    let answer = [];
+// function solution(X,Y){
+//     let answer = [];
     
-    let countX = new Array(10).fill(0);
-    let countY = new Array(10).fill(0);
+//     let countX = new Array(10).fill(0);
+//     let countY = new Array(10).fill(0);
     
-    const xArr = X.split("");
-    const yArr = Y.split("");
+//     const xArr = X.split("");
+//     const yArr = Y.split("");
     
-    xArr.forEach((item) => {
-        countX[item]++;
-    })
+//     xArr.forEach((item) => {
+//         countX[item]++;
+//     })
         
-    yArr.forEach((item) => {
-        countY[item]++;
-    })
+//     yArr.forEach((item) => {
+//         countY[item]++;
+//     })
     
     
-    countX.forEach((item, index) => {
-        if(item !== 0 && countY[index] !== 0){
-            const min = Math.min(item, countY[index]);
+//     countX.forEach((item, index) => {
+//         if(item !== 0 && countY[index] !== 0){
+//             const min = Math.min(item, countY[index]);
             
-            answer.push(String(index).repeat(min));
-        }
-    })
+//             answer.push(String(index).repeat(min));
+//         }
+//     })
     
     
-    if(answer.length === 0){
-        return "-1";
+//     if(answer.length === 0){
+//         return "-1";
+//     }
+    
+//     if(Number(answer.join("")) === 0){
+//         return "0";
+//     }
+    
+//     return answer.reverse().join("");
+// }
+
+function solution(X, Y) {
+    let result = '';
+    const numObj = {};
+
+    for (const char of X) {
+        numObj[char] = (numObj[char] || 0) + 1;
     }
-    
-    if(Number(answer.join("")) === 0){
-        return "0";
+
+    for (const char of Y) {
+        if (!numObj[char]) continue;
+        result += char;
+        numObj[char]--;
     }
-    
-    return answer.reverse().join("");
+
+    if (result === '') return '-1';
+    if (+result === 0) return '0';
+    return [...result]
+        .map((num) => +num)
+        .sort((a, b) => b - a)
+        .join('');    
 }
