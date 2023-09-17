a12aca4838c54e8f7bf0cdf08e1f370637c059e9
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-09-17 23:36:35 +0900

    [level 1] Title: 폰켓몬, Time: 0.23 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,10 +1,18 @@
-function solution(nums) {
-    let noDuplication = [];
+// function solution(nums) {
+//     let noDuplication = [];
     
-    for(let i=0; i<nums.length; i++){
-        if(!noDuplication.includes(nums[i])){
-            noDuplication.push(nums[i])
-        }
-    }
-    return noDuplication.length > (nums.length)/2 ? (nums.length)/2 : noDuplication.length
+//     for(let i=0; i<nums.length; i++){
+//         if(!noDuplication.includes(nums[i])){
+//             noDuplication.push(nums[i])
+//         }
+//     }
+//     return noDuplication.length > (nums.length)/2 ? (nums.length)/2 : noDuplication.length
+// }
+
+
+function solution(nums) {
+  const max = nums.length / 2;
+  const arr = [...new Set(nums)];
+
+  return arr.length > max ? max : arr.length
 }
