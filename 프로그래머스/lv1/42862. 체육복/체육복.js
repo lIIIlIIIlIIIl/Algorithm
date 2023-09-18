9109599fad126daa2b583c26566f0a6f92183d8c
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-09-18 23:27:47 +0900

    [level 1] Title: 체육복, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,18 +1,58 @@
+// function solution(n, lost, reserve) {
+
+//     let answer = 0;
+
+//     let hasUniform = new Array(n).fill(1);
+
+//     for (let i = 0; i < lost.length; i++) {
+//         hasUniform[lost[i] - 1]--;
+//     }
+
+//     for (let i = 0; i < reserve.length; i++) {
+//         hasUniform[reserve[i] - 1]++;
+//     }
+
+//     for (let i = 0; i < hasUniform.length; i++) {
+//         if (hasUniform[i] === 0) {
+//             if (hasUniform[i - 1] === 2) {
+//                 hasUniform[i]++;
+//                 hasUniform[i - 1]--;
+//             } else if (hasUniform[i + 1] === 2) {
+//                 hasUniform[i]++;
+//                 hasUniform[i + 1]--;
+//             }
+//         }
+
+//         if (hasUniform[i] >= 1) {
+//             answer++;
+//         }
+//     }
+
+//     return answer;
+// }
+
 function solution(n, lost, reserve) {
-    const clothes = Array(n).fill(1);
+    const students = {};
+    let answer = 0;
+    for(let i = 1; i <= n; i++){
+        students[i] = 1;
+    }
+    lost.forEach(number => students[number] -= 1);
+    reserve.forEach(number => students[number] += 1);
 
-    lost.map((lost) => {clothes[lost-1] = 0});
-    reserve.map(reserve => {clothes[reserve-1] += 1});
-
-    for(let i=0; i<n; i++){
-        if(clothes[i] === 0 && clothes[i-1] ===2){
-            clothes[i] = 1;
-            clothes[i-1] = 1;
-        }
-        else if(clothes[i] === 0 && clothes[i+1] === 2){
-            clothes[i] = 1;
-            clothes[i+1]=1;
+    for(let i = 1; i <= n; i++){
+        if(students[i] === 2 && students[i-1] === 0){
+                students[i-1]++;
+                students[i]--;
+        } else if(students[i] === 2 && students[i+1] === 0){
+                students[i+1]++;
+                students[i]--;
         }
     }
-    return clothes.filter(c => c > 0).length;
+    for(let key in students){
+        if(students[key] >= 1){
+            answer++;
+        }
+    }
+    return answer;
 }
