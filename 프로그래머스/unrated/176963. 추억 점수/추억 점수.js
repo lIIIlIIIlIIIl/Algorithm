e6b5e9a1494908120353c1bbf1a1fb087f241d3b
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-09-20 23:39:50 +0900

    [unrated] Title: 추억 점수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -1,19 +1,29 @@
+// function solution(name, yearning, photo) {
+//     let answer = [];
+//     let nameAndScore = {};
+
+//     for(let i=0; i<name.length; i++){
+//         nameAndScore[name[i]] = yearning[i];
+//     }
+
+
+//     photo.map((pt) => {
+//         let score = 0;
+//         for(let i=0; i<pt.length; i++){
+//             let personScore = nameAndScore[pt[i]];
+//             personScore && (score += personScore)
+//         }
+//          answer.push(score)
+//     })
+//     return answer;
+// }
+
+
+
 function solution(name, yearning, photo) {
-    let answer = [];
-    let nameAndScore = {};
-
-    for(let i=0; i<name.length; i++){
-        nameAndScore[name[i]] = yearning[i];
+    let obj = {};
+    for(let i = 0; i < name.length; i++){
+        obj[name[i]] = yearning[i];
     }
-
-
-    photo.map((pt) => {
-        let score = 0;
-        for(let i=0; i<pt.length; i++){
-            let personScore = nameAndScore[pt[i]];
-            personScore && (score += personScore)
-        }
-         answer.push(score)
-    })
-    return answer;
+    return photo.map(value => value.map(v => obj[v] ? obj[v] : 0).reduce((acc,cur) => acc + cur,0))
 }
