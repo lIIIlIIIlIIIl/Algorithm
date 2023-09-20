e9a8a87ef21500d9bec8c2405eeb8ed43b5a1761
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-09-20 23:40:04 +0900

    [unrated] Title: 추억 점수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -1,29 +1,19 @@
-// function solution(name, yearning, photo) {
-//     let answer = [];
-//     let nameAndScore = {};
-
-//     for(let i=0; i<name.length; i++){
-//         nameAndScore[name[i]] = yearning[i];
-//     }
-
-
-//     photo.map((pt) => {
-//         let score = 0;
-//         for(let i=0; i<pt.length; i++){
-//             let personScore = nameAndScore[pt[i]];
-//             personScore && (score += personScore)
-//         }
-//          answer.push(score)
-//     })
-//     return answer;
-// }
-
-
-
 function solution(name, yearning, photo) {
-    let obj = {};
-    for(let i = 0; i < name.length; i++){
-        obj[name[i]] = yearning[i];
+    let answer = [];
+    let nameAndScore = {};
+
+    for(let i=0; i<name.length; i++){
+        nameAndScore[name[i]] = yearning[i];
     }
-    return photo.map(value => value.map(v => obj[v] ? obj[v] : 0).reduce((acc,cur) => acc + cur,0))
+
+
+    photo.map((pt) => {
+        let score = 0;
+        for(let i=0; i<pt.length; i++){
+            let personScore = nameAndScore[pt[i]];
+            personScore && (score += personScore)
+        }
+         answer.push(score)
+    })
+    return answer;
 }
