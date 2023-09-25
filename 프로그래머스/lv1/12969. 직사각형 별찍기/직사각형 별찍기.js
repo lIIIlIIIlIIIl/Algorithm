e71033e6a6e174364554176872daf2abb4a7ad7f
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-09-25 18:05:40 +0900

    [level 1] Title: 직사각형 별찍기, Time: 62.72 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -1,12 +1,18 @@
+// process.stdin.setEncoding('utf8');
+// process.stdin.on('data', data => {
+//     const n = data.split(" ");
+//     const a = Number(n[0]), b = Number(n[1]);
+//     console.log(a);
+//     console.log(b);
+// });
+
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    for(let i=0; i<b; i++){
-        let answer = "";
-        for(let j=0; j<a; j++){
-            answer += "*"
-        }
-        console.log(answer);
+    const row = '*'.repeat(a)
+    for(let i =0; i < b; i++){
+        console.log(row)
     }
+
 });
