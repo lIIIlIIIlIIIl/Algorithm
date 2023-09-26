72bec309c8827bc5624d1d3fa2f083353ccfa58e
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-09-26 21:57:04 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,14 +1,28 @@
+// function solution(x) {
+//     // 자릿수의 합 구하기
+//     // 합으로 x를 나눴을 때 나눠 떨어지면 true 아니면 false
+    
+//     let answer = true;
+    
+//     let sum = 0
+    
+//     x.toString().split("").map((el)=>sum += parseInt(el));
+    
+//     x % sum === 0 ? answer = true : answer = false;
+    
+//     return answer;
+// }
+
 function solution(x) {
-    // 자릿수의 합 구하기
-    // 합으로 x를 나눴을 때 나눠 떨어지면 true 아니면 false
+    let num = x;
+    let sum = 0;
     
-    let answer = true;
+    do {
+        sum += x % 10;
+        x = Math.floor(x / 10);
+    } 
     
-    let sum = 0
-    
-    x.toString().split("").map((el)=>sum += parseInt(el));
-    
-    x % sum === 0 ? answer = true : answer = false;
-    
-    return answer;
+    while (x>0);
+
+    return !(num%sum);
 }
