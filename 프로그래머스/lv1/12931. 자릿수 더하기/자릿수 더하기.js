fa2d8ffa98c376ad86e8b65ad8f087d782d7707f
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-09-29 22:16:03 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,11 +1,15 @@
-function solution(n)
-{
-    let answer = 0;
-    let str = String(n);
-    let numArray = [...str];
+// function solution(n)
+// {
+//     let answer = 0;
+//     let str = String(n);
+//     let numArray = [...str];
     
-    for(let el of numArray){
-        answer += Number(el);
-    }
-    return answer;
+//     for(let el of numArray){
+//         answer += Number(el);
+//     }
+//     return answer;
+// }
+
+function solution(n){
+    return (n + "").split("").reduce((acc, curr) => acc + parseInt(curr), 0)
 }
