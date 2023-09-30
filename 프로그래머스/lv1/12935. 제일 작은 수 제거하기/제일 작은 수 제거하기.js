1737a9b8768f5cf9d130298e0a180a4bec5b170c
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-09-30 19:35:47 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,12 +1,17 @@
-function solution(arr){
-    if(arr.length < 2) return [-1];
+// function solution(arr){
+//     if(arr.length < 2) return [-1];
     
-    let min = Math.min(...arr);
+//     let min = Math.min(...arr);
     
-    return arr.filter((num) => num !== min);
+//     return arr.filter((num) => num !== min);
+// }
+
+
+function solution(arr) {
+    arr.splice(arr.indexOf(Math.min(...arr)),1);
+    if(arr.length<1)return[-1];
+    return arr;
 }
-
-
 
 
 
