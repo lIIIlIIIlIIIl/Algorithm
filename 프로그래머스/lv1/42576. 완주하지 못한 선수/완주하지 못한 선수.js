8d3cba1cada38d0a37e93d4c05da04e351d3a482
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-10-01 23:06:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 26.15 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,30 +1,48 @@
+// function solution(participant, completion) {
+//     let person = {};
+//     let answer = "";
+    
+//     participant.forEach((p) => {
+//         if(!person[p]){
+//             person[p] = 1;
+//             return;
+//         }
+//         if(person[p]){
+//             person[p]++;
+//             return;
+//         }
+//     })
+    
+//     completion.forEach((p) => {
+//         if(person[p]){
+//             person[p]--;
+//             return;
+//         }
+//     })
+    
+//     for(let key in person){
+//         if(person[key] === 1){
+//             answer += key;
+//         }
+//     }
+    
+//     return answer;
+// }
+
 function solution(participant, completion) {
-    let person = {};
-    let answer = "";
-    
-    participant.forEach((p) => {
-        if(!person[p]){
-            person[p] = 1;
-            return;
-        }
-        if(person[p]){
-            person[p]++;
-            return;
-        }
-    })
-    
-    completion.forEach((p) => {
-        if(person[p]){
-            person[p]--;
-            return;
-        }
-    })
-    
-    for(let key in person){
-        if(person[key] === 1){
-            answer += key;
-        }
+    const map = new Map();
+
+    for(let i = 0; i < participant.length; i++) {
+        let a = participant[i], 
+            b = completion[i];
+
+        map.set(a, (map.get(a) || 0) + 1);
+        map.set(b, (map.get(b) || 0) - 1);
     }
-    
-    return answer;
+
+    for(let [k, v] of map) {
+        if(v > 0) return k;
+    }
+
+    return 'nothing';
 }
