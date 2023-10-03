ee3f965c84932be03a565a2c9b19911bb28c1b38
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-10-03 23:42:58 +0900

    [unrated] Title: 문자열 돌리기, Time: 55.97 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.js
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.js
@@ -1,3 +1,17 @@
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// let input = [];
+
+// rl.on('line', function (line) {
+//     input = line
+// }).on('close',function(){
+//     [...input].forEach(a => console.log(a))
+// });
+
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
@@ -7,7 +21,8 @@ const rl = readline.createInterface({
 let input = [];
 
 rl.on('line', function (line) {
-    input = line
+    input = [line];
 }).on('close',function(){
-    [...input].forEach(a => console.log(a))
+    str = input[0];
+    [...str].forEach(c => console.log(c))
 });
