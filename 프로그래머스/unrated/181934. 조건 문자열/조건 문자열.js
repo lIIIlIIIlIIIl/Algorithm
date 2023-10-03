6a80672f77d776d2a8d833d2843482a1cbc86013
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-10-03 23:43:57 +0900

    [unrated] Title: 조건 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -1,17 +1,29 @@
+// function solution(ineq, eq, n, m) {
+//     if(ineq === "<" && eq === "="){
+//         return n <= m ? 1 : 0;
+//     }
+//     if(ineq === "<" && eq === "!"){
+//         return n < m ? 1 : 0;
+        
+//     }
+//     if(ineq === ">" && eq === "="){
+//         return n >= m ? 1 : 0;
+        
+//     }
+//     if(ineq === ">" && eq === "!"){
+//         return n > m ? 1 : 0;
+        
+//     }
+// }
+
+const operations = {
+  '>=': (n, m) => n >= m,
+  '<=': (n, m) => n <= m,
+  '>!': (n, m) => n > m,
+  '<!': (n, m) => n < m,
+};
+
 function solution(ineq, eq, n, m) {
-    if(ineq === "<" && eq === "="){
-        return n <= m ? 1 : 0;
-    }
-    if(ineq === "<" && eq === "!"){
-        return n < m ? 1 : 0;
-        
-    }
-    if(ineq === ">" && eq === "="){
-        return n >= m ? 1 : 0;
-        
-    }
-    if(ineq === ">" && eq === "!"){
-        return n > m ? 1 : 0;
-        
-    }
+  const op = operations[ineq + eq];
+  return Number(op(n, m));
 }
