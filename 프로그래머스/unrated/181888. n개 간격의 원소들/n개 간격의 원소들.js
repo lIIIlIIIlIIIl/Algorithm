970d895ce48b417568a8b967b2720f0d53640cc7
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-10-04 23:13:15 +0900

    [unrated] Title: n개 간격의 원소들, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -1,3 +1,6 @@
-function solution(num_list, n) {
-    return num_list.filter((_, index) => index % n === 0);
-}
+// function solution(num_list, n) {
+//     return num_list.filter((_, index) => index % n === 0);
+// }
+
+
+const solution = (num_list, n) => num_list.filter((_, i) => !(i % n))
