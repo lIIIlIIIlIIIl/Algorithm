a09f49bce9b90597d8e14bd2924518b07ec76e27
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-10-06 23:52:41 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 4.75 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,12 +1,21 @@
+// function solution(arr, queries) {
+//     return queries.reduce((acc, cur) => {
+//         const copyArr = [...acc]
+//         const [to, from, val] = cur
+//         return copyArr.map((el, elIdx) => {
+//             if(elIdx >= to && elIdx <= from) {
+//                 if(elIdx%val === 0) return el+1
+//             }
+//             return el
+//         })
+//     }, arr)
+// }
+
 function solution(arr, queries) {
-    return queries.reduce((acc, cur) => {
-        const copyArr = [...acc]
-        const [to, from, val] = cur
-        return copyArr.map((el, elIdx) => {
-            if(elIdx >= to && elIdx <= from) {
-                if(elIdx%val === 0) return el+1
-            }
-            return el
-        })
-    }, arr)
+    for(let [s, e, k] of queries) {
+        for(let i = s; i <= e; i++) {
+            if(i % k === 0) arr[i]++;
+        }
+    }
+    return arr;
 }
