beb551cc6e8249927cdd087bdde21ae0f4a74612
Shin-Dong-Jae <sdhsdj6450@gmail.com>
2023-10-06 23:52:15 +0900

    [unrated] Title: 코드 처리하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -1,14 +1,25 @@
-function solution(code) {
-    let mode = 0
-    return [...code].reduce((acc, cur, idx) => {
-        if(mode) {
-            if(cur !== '1' && idx%2 === 1)  return acc+cur
-            if(cur === '1') mode = 0
-        } else {
-            if(cur !== '1' && idx%2 === 0)  return acc+cur
-            if(cur === '1') mode = 1
-        }
+// function solution(code) {
+//     let mode = 0
+//     return [...code].reduce((acc, cur, idx) => {
+//         if(mode) {
+//             if(cur !== '1' && idx%2 === 1)  return acc+cur
+//             if(cur === '1') mode = 0
+//         } else {
+//             if(cur !== '1' && idx%2 === 0)  return acc+cur
+//             if(cur === '1') mode = 1
+//         }
             
-        return acc
-    }, "") || "EMPTY"
+//         return acc
+//     }, "") || "EMPTY"
+// }
+
+function solution(code) {
+    let odd = false
+    return Array.from(code).reduce((acc, v, i) => {
+        if (v === '1') {
+            odd = !odd
+            return acc
+        }
+        return (i % 2 === (odd ? 1 : 0)) ? acc + v : acc
+    }, '') || 'EMPTY'
 }
